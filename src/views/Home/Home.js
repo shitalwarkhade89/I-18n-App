2cf7ddf312dfa56ee96b34e6ade4f5a88ad6ead2
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-11-21 11:06:34 -0800

    initial commit

---
+++ src/views/Home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <div>
+                <h1>Welcome</h1>
+                <p>
+                    We hope you enjoy learning React
+                </p>
+                <h3>
+                    Have a great day!
+                </h3>
+            </div>
+        </>
+    )
+}
+export default Home
+
