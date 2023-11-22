b8da56a94182b658131f7cd2c5b23e83d7f1e670
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-11-21 23:33:13 -0800

    some modifications in css

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,34 +1,38 @@
 import React, { useState } from "react";
-import './Home.css'
+import './Home.css';
 import I18n from "./../../utils/i18n";
 
 function Home() {
-    const usersCount =30;
+    const usersCount = 30;
 
-    
+
     return (
         <>
-            <div>
-                <h1>{I18n("welcomeMessage")}</h1>
-                <p>{I18n("normalMessage")}</p>
+        <h1 className="heading">React I18n</h1>
+            <div className="main-container">
+                <div>
+                    <h1 >{I18n("welcomeMessage")}</h1>
+                    <h4>{I18n("normalMessage")}</h4>
 
-                <h3>{I18n("greetingMessage")}</h3>
-                <h5>{I18n("endMessage")}</h5>
-                <select
-                 defaultValue={localStorage.getItem("lang")}
-                 onChange={(e)=>{
-                    localStorage.setItem("lang",e.target.value);
-                    window.location.reload();
-                }}>
-                    <option value={"mr"}>मराठी</option>
-                    <option value={"hi"}>हिंदी</option>
-                    <option value={"en"}>English</option>
-                    
-                </select>
+                    <h3>{I18n("greetingMessage")}</h3>
+                    <h4>{I18n("endMessage")}</h4>
+                    <h4>{I18n("usersStatMessage", "<studentCount>", usersCount)} </h4>
+                </div>
 
-                
+                <div>
+                    <h4>{I18n("selectLang")}</h4>
+                    <select className="select-bar"
+                        defaultValue={localStorage.getItem("lang")}
+                        onChange={(e) => {
+                            localStorage.setItem("lang", e.target.value);
+                            window.location.reload();
+                        }}>
+                        <option value={"mr"}>मराठी</option>
+                        <option value={"hi"}>हिंदी</option>
+                        <option value={"en"}>English</option>
 
-                <p>{I18n("usersStatMessage","<studentCount>", usersCount) } </p>
+                    </select>
+                </div>
             </div>
         </>
     )
